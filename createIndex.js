d2b62ac5cbf1cf580ed02d8ae77f4b8839c4336e
Darrell <draper.darrell@gmail.com>
2023-12-06 11:32:07 -0800

    cleaned up createIndex.js

--- createIndex.js
+++ createIndex.js
@@ -1,9 +1,7 @@
 //createIndex.js
 
-
-
+//import packages
 import * as fs from 'fs';
-// hello_algolia.js
 import algoliasearch from 'algoliasearch';
 
 // Connect and authenticate with your Algolia app
@@ -14,18 +12,8 @@ const index = client.initIndex('movies')
 
 //Load JSON data (movies)
 var obj = JSON.parse(fs.readFileSync('datasets/movies/movies.json', 'utf8'));
-console.log('tpe: ' + typeof(obj))
-//var otherobj = JSON.parse(obj)
-//console.log('tpe2: ' + typeof(otherobj));
-console.log(obj[0]);
-console.log("lenght: " + obj.length)
 
-var remainder = obj.length%100
-
-console.log(remainder)
-console.log(obj.length-remainder-1)
-
-
+//batch records for saving
 for (let x = 0; x < (obj.length-remainder); x+=100) {
 
 	var smallobj = obj.slice(x,x+100);
@@ -33,33 +21,3 @@ for (let x = 0; x < (obj.length-remainder); x+=100) {
   console.log(objectIDs);
 });
 }
-
-var finalobj = obj.slice(obj.length-remainder,obj.length )
-
-console.log('last: ' + finalobj.length)
-
-
-
-/*
-
-for (let x in obj) {
-	index.saveObject(obj[x])
-}
-
-*/
-
-console.log('loadded');
-
-/*
-
-*/
-
-
-// Search the index and print the results
-/*index
-  .search('test_record')
-  .then(({ hits }) => console.log(hits[0]))
-*/
-
-
-//console.log(obj[0]);
